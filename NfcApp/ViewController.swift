38ef57244ba5cb32cfab0c2932b2d1ec9220f857
Constantine Mars <constantine.mars@gmail.com>
2021-02-17 12:36:38 -0800

    Initial Commit

---
+++ NfcApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NfcApp
+//
+//  Created by Constantine Mars on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

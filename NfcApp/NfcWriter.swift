76c1dc31313619c632e431704cd8e449933c7325
Constantine Mars <constantine.mars@gmail.com>
2021-02-21 00:06:33 -0800

    init

---
+++ NfcApp/NfcWriter.swift
@@ -0,0 +1,13 @@
+//
+//  NfcWriter.swift
+//  NfcApp
+//
+//  Created by Constantine Mars on 2/20/21.
+//
+
+import Foundation
+import CoreNFC
+
+class NfcWriter: NSObject, NFCNDEFReaderSessionDelegate {
+    
+}
